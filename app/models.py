41f5d05c19da1f3b43682cc2fe57876294d4503f
Анна Хаманаева <hamanaevaanna@gmail.com>
2026-09-05 13:08:25 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import ForeignKey, String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database import Base
@@ -15,3 +15,28 @@ class Department(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+class Room(Base):
+    __tablename__ = "rooms"
+
+    __table_args__ = (
+        UniqueConstraint(
+            "building_id",
+            "number",
+            name="uq_room_building_number",
+        ),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    number: Mapped[str] = mapped_column(String(20), nullable=False)
+    area: Mapped[float] = mapped_column(nullable=False)
+    capacity: Mapped[int] = mapped_column(nullable=False)
+
+    building_id: Mapped[int] = mapped_column(
+        ForeignKey("buildings.id"),
+        nullable=False,
+    )
+    department_id: Mapped[int] = mapped_column(
+        ForeignKey("departments.id"),
+        nullable=False,
+    )
