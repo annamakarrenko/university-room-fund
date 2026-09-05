98f29d8346f5e91cb39fe2816a83ebc3cd82c917
Анна Хаманаева <hamanaevaanna@gmail.com>
2026-09-05 13:09:58 +0300

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 class BuildingCreate(BaseModel):
     name: str
@@ -24,5 +24,31 @@ class DepartmentUpdate(BaseModel):
 class DepartmentResponse(BaseModel):
     id: int
     name: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+class RoomCreate(BaseModel):
+    number: str
+    area: float = Field(gt=0)
+    capacity: int = Field(ge=0)
+    building_id: int
+    department_id: int
+
+
+class RoomUpdate(BaseModel):
+    number: str
+    area: float = Field(gt=0)
+    capacity: int = Field(ge=0)
+    building_id: int
+    department_id: int
+
+
+class RoomResponse(BaseModel):
+    id: int
+    number: str
+    area: float
+    capacity: int
+    building_id: int
+    department_id: int
 
     model_config = ConfigDict(from_attributes=True)
