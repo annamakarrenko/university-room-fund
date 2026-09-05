04f799b64dfb95c3b6747397633d8f529d05ce49
0Darya <minidasha07@gmail.com>
2026-09-05 12:46:18 +0300

    Add Department database model

--- app/models.py
+++ app/models.py
@@ -9,3 +9,9 @@ class Building(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     address: Mapped[str] = mapped_column(String(255), nullable=False)
+
+class Department(Base):
+    __tablename__ = "departments"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
